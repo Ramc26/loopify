2bba251d46e1fefd5309dbac96a80df602d4f4ac
ramc26 <rakeshchowdary885@gmail.com>
2025-11-06 15:26:06 +0530

    Refactor main.py to streamline execution by removing the main function and directly calling render_ui. Update version in uv.lock from 0.1.0 to 1.0.0.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import streamlit as st
 from ui import render_ui
 
-def main():
-    st.set_page_config(
-        page_title="Loopify",
-        page_icon="➰",
-        layout="wide",
-    )
-    render_ui()
+st.set_page_config(
+    page_title="Loopify",
+    page_icon="➰",
+    layout="wide",
+)
 
-if __name__ == "__main__":
-    main()
+render_ui()

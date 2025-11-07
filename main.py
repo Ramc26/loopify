c1cad0cc345ccad4cf1d70b2709a2560b77491e0
ramc26 <rakeshchowdary885@gmail.com>
2025-11-07 11:06:29 +0530

    Add Open Graph and Twitter meta tags to enhance SEO and sharing capabilities in main.py

--- main.py
+++ main.py
@@ -7,4 +7,21 @@ st.set_page_config(
     layout="wide",
 )
 
+st.markdown(
+    """
+    <head>
+        <meta property="og:title" content="Loopify - Automate Your API Calls" />
+        <meta property="og:description" content="A powerful Streamlit app for testing APIs, executing single requests, and running batch operations. Import from cURL, test endpoints, and automate your API workflows." />
+        <meta property="og:image" content="https://github.com/Ramc26/loopify/blob/master/assets/loopify.svg" />
+        <meta property="og:url" content="https://loopify.streamlit.app//" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Loopify - Automate Your API Calls" />
+        <meta name="twitter:description" content="A powerful Streamlit app for testing APIs, executing single requests, and running batch operations." />
+        <meta name="twitter:image" content="https://github.com/Ramc26/loopify/blob/master/assets/loopify.svg" />
+    </head>
+    """,
+    unsafe_allow_html=True,
+)
+
 render_ui()
